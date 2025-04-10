39e5bfb63471f0a7c726e13be45245e824cef454
thiago <thiagoagostinho6@gmail.com>
2025-04-09 21:35:44 -0300

    Adiciona impressão dos dados iniciais da conta bancária

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class ContaBancaria {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String nome = "Thiago Agostinho";
+        String tipoConta = "corrente";
+        double saldo = 1500;
+
+        System.out.println("*".repeat(20));
+        System.out.println("Dados da conta bancária do cliente:\n");
+        System.out.println("Nome: " + nome);
+        System.out.println("Tipo da conta: " + tipoConta);
+        System.out.println("Saldo inicial: " + saldo);
+        System.out.println("*".repeat(20));
+
     }
 }
